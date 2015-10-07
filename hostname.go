5eb4ef3c544ef2bca0a72d7e9832428970bdde86
Antonio Murdaca <amurdaca@redhat.com>
2015-10-07 17:36:53 +0200

    handle old openssl version
    Signed-off-by: Antonio Murdaca <amurdaca@redhat.com>

--- hostname.go
+++ hostname.go
@@ -20,6 +20,18 @@ package openssl
 #include <openssl/ssl.h>
 #include <openssl/conf.h>
 #include <openssl/x509v3.h>
+
+#ifndef X509_CHECK_FLAG_ALWAYS_CHECK_SUBJECT
+#define X509_CHECK_FLAG_ALWAYS_CHECK_SUBJECT	0x1
+#define X509_CHECK_FLAG_NO_WILDCARDS	0x2
+
+extern int X509_check_host(X509 *x, const char *chk, size_t chklen,
+    unsigned int flags, char **peername);
+extern int X509_check_email(X509 *x, const char *chk, size_t chklen,
+    unsigned int flags);
+extern int X509_check_ip(X509 *x, const unsigned char *chk, size_t chklen,
+		unsigned int flags);
+#endif
 */
 import "C"
 
